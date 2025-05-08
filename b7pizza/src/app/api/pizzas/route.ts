386695f5a0c4849ad993cfa6b33aada464e8bcd4
Borges dev <leosull50@hotmail.com>
2025-05-08 18:04:32 -0300

    feat: configuring axios and making request

---
+++ b7pizza/src/app/api/pizzas/route.ts
@@ -0,0 +1,12 @@
+import { getAllProducts } from "@/services/product";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    let pizzas = await getAllProducts()
+
+    pizzas = pizzas.map((pizza: any) => ({
+        ...pizza,
+        image: `${process.env.NEXT_PUBLIC_BASE_URL}/pizzas/${pizza.image}`
+    }))
+    return NextResponse.json({pizzas})
+}
---
+++ b7pizza/src/components/home/pizza-list.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Product } from "@/generated/prisma";
+
+type Props = {
+  pizzas: Product[];
+};
+
+export const PizzaList = ({ pizzas }: Props) => {
+  return <div>Total: {pizzas.length}</div>;
+};
